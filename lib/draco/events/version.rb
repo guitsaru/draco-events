a819b5d0d60fd4ebae864eaafbd47e5dee00372d
Matt Pruitt <matt@guitsaru.com>
2021-01-03 21:04:14 +0700

    Initial commit

---
+++ lib/draco/events/version.rb
@@ -0,0 +1,5 @@
+module Draco
+  module Events
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/draco/events_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Draco::Events do
+  it "has a version number" do
+    expect(Draco::Events::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
